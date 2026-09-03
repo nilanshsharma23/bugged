022032960272a2143e59580ec4e4d026bdf7e9f2
Nilansh Sharma <80972022+nilanshsharma23@users.noreply.github.com>
2026-09-03 22:19:28 +0530

    idk made the navbar or something, got some style right

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let { children, onclick } = $props();
+</script>
+
+<button
+	{onclick}
+	class="cursor-pointer rounded-xl border-2 border-black bg-emerald-600 px-8 py-2 text-3xl text-white shadow-[3px_3px_0px_#000] transition-all hover:shadow-[0px_0px_0px_#000]"
+	>{@render children?.()}</button
+>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,13 @@
-<h1>Bugged!</h1>
+<script lang="ts">
+	import flag from '$lib/assets/flag.svg';
+	import Button from '$lib/components/Button.svelte';
+
+	const rsvpButton = () => {
+		window.open('https://rsvp.hackclub.community/bugged', '_black')?.focus();
+	};
+</script>
+
+<nav class="flex flex-row items-center justify-between px-8">
+	<img src={flag} class="w-40" alt="" />
+	<Button onclick={rsvpButton}>RSVP</Button>
+</nav>
